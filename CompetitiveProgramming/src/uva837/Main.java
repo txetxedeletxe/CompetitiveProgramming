3b401ad7507cf21abb3fd36ff12720f90a4afee6
sophyelord <sophyelord@DESKTOP-6RFLPF0>
2018-07-03 15:02:35 +0200

    update

--- CompetitiveProgramming/src/uva837/Main.java
+++ CompetitiveProgramming/src/uva837/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
