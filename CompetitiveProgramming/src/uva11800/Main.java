2b675fe90cd1902623b507268e3ea97b6a9d26e7
sophy <sophy@sophyLaptop>
2018-07-05 18:12:02 +0200

    porfin

--- CompetitiveProgramming/src/uva11800/Main.java
+++ CompetitiveProgramming/src/uva11800/Main.java
@@ -1,28 +1,21 @@
 package uva11800;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.OutputStreamWriter;
 
 class Main {
 
 	
-	static final Comparator<Integer[]> cmp= new Comparator<Integer[]>() {
-
-		@Override
-		public int compare(Integer[] o1, Integer[] o2) {
-			
-			return o1[1].compareTo(o2[1]);
-		}};
-	
+	static double lastAngle = 0;
 	public static void main(String[] args) {
 		
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = null;
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String  str = null;
 		try {
 			str = br.readLine();
 		} catch (IOException e) {
@@ -32,8 +25,8 @@ class Main {
 
 		int cases = Integer.parseInt(str);
 		Integer points[][] = new Integer[4][2];
-		float vectors[][] = new float[4][2];
-		float sideLengths[] = new float[4];
+		int vectors[][] = new int[4][2];
+		int sideLengths[] = new int[4];
 		
 		for (int i = 1 ; i <= cases ; i++) {
 		
@@ -52,25 +45,22 @@ class Main {
 				points[j][1] = Integer.parseInt(splited[1]);
 			}
 			
-			Arrays.sort(points, cmp);
-			
-			if ((points[0][0] > points[1][0] && points[2][0] < points[3][0]) || (points[0][0] < points[1][0] && points[2][0] > points[3][0])) {
-				swap(points,0,1);
-			}
+			swap(points,0,maxX(points));
+			sort(points);
 			
 			
 			
 			vectors[0][0] = points[1][0] - points[0][0];
 			vectors[0][1] = points[1][1] - points[0][1];
 			
-			vectors[1][0] = points[3][0] - points[2][0];
-			vectors[1][1] = points[3][1] - points[2][1];
+			vectors[1][0] = points[2][0] - points[1][0];
+			vectors[1][1] = points[2][1] - points[1][1];
 			
-			vectors[2][0] = points[2][0] - points[0][0];
-			vectors[2][1] = points[2][1] - points[0][1];
+			vectors[2][0] = points[3][0] - points[2][0];
+			vectors[2][1] = points[3][1] - points[2][1];
 			
-			vectors[3][0] = points[3][0] - points[1][0];
-			vectors[3][1] = points[3][1] - points[1][1];
+			vectors[3][0] = points[0][0] - points[3][0];
+			vectors[3][1] = points[0][1] - points[3][1];
 			
 			
 			sideLengths[0] = vectors[0][0]*vectors[0][0] + vectors[0][1]*vectors[0][1];
@@ -79,71 +69,100 @@ class Main {
 			sideLengths[3] = vectors[3][0]*vectors[3][0] + vectors[3][1]*vectors[3][1];
 		
 			
-			if (sideLengths[0] == sideLengths[1]) {
-				if (sideLengths[2] == sideLengths[3]) {
-					if (sideLengths[0] == sideLengths[2]) {
-						if (dotProduct(vectors[0], vectors[2]) == 0) {
-							figure = "Square";
-						}
-						else {
-							figure = "Rhombus";
-						}
-						
-					}
-					else {
-						if (dotProduct(vectors[0], vectors[2]) == 0) {
-							figure = "Rectangle";
-						}
-						else {
-							figure = "Parallelogram";
-						}
-					}
-				}else {
-					double dp = dotProduct(vectors[0],vectors[1]);
-					if (dp*dp == sideLengths[0]*sideLengths[1]) {
-						figure ="Trapezium";
-					}
-					else {
-						dp = dotProduct(vectors[2], vectors[3]);
-						if (dp*dp == sideLengths[2]*sideLengths[3]) {
-							figure = "Trapezium";
-						}
-						else
-							figure = "Ordinary Quadrilateral";
-					}
-						
+			if (sideLengths[0] == sideLengths[2] && sideLengths[1] == sideLengths[3]) {
+				
+				if (sideLengths[0] == sideLengths[1]) {
 					
-				}
-			}
-			else{
-				double dp = dotProduct(vectors[0],vectors[1]);
-				if (dp*dp == sideLengths[0]*sideLengths[1]) {
-					figure ="Trapezium";
+					if (perpendicular(vectors[0],vectors[1])) {
+						figure = "Square";
+					}
+					else {
+						figure = "Rhombus";
+					}
 				}
 				else {
-					dp = dotProduct(vectors[2], vectors[3]);
-					if (dp*dp == sideLengths[2]*sideLengths[3]) {
-						figure = "Trapezium";
-					}
-					else
-						figure = "Ordinary Quadrilateral";
-				}
 					
+					if (perpendicular(vectors[0],vectors[1])) {
+						figure = "Rectangle";
+					}
+					else {
+						figure = "Parallelogram";
+					}
+				}
+			}
+			else {
 				
+				if (parallel(vectors[0], vectors[2])|| parallel(vectors[1], vectors[3]))
+					figure = "Trapezium";
+				else
+					figure = "Ordinary Quadrilateral";
 			}
 			
-			
-			System.out.println("Case " + i + ": " + figure);
+			try {
+				bw.write("Case " + i + ": " + figure + "\n");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 		}
+		try {
+			bw.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
-	private static float dotProduct(float[] ds, float[] ds2) {
+	private static int maxX(Integer[][] points) {
+		
+		int max = 0;
+		for (int i = 0 ; i < points.length ; i++) {
+			
+			if (earlier(points[i],points[max]))
+				max = i;
+		}
+		return max;
+	}
+
+	private static boolean earlier(Integer[] integers, Integer[] integers2) {
+		
+		
+			return integers[0] > integers2[0];
+		
+	}
+
+	private static void sort(Integer[][] points) {
+		
+		lastAngle = 0;
+		int sp = ccwisePoint(points, 0);
+		swap(points, 1, sp);
+		sp = ccwisePoint(points, 1);
+		swap(points, 2, sp);
+		
+		
+	}
+
+	private static int dotProduct(int[] ds, int[] ds2) {
 		
 		return ds[0]*ds2[0] + ds[1]*ds2[1];
 	}
 
+	private static int crossProduct(int[] ds, int[] ds2) {
+		
+		return ds[0]*ds2[1] - ds[1]*ds2[0];
+	}
+	
+	private static boolean parallel(int[] ds, int[] ds2) {
+		return crossProduct(ds, ds2) == 0;
+	}
+	
+	private static boolean perpendicular(int[] ds, int[] ds2) {
+		return dotProduct(ds, ds2) == 0;
+	}
+	
 	private static void swap(Integer[][] points, int i, int j) {
 		
 		Integer[] temp = points[i];
@@ -151,5 +170,36 @@ class Main {
 		points[j] = temp;
 		
 	}
+	
+	private static int ccwisePoint(Integer[][] points, int spoint) {
+		
+		double angle = Double.POSITIVE_INFINITY;
+		int smallest = spoint+1;
+		for (int i = smallest ; i < points.length ; i++) {
+			
+			double nangle = angleAt(points[spoint],points[i]);
+			if (nangle < angle) {
+				angle = nangle;
+				smallest = i;
+			}
+		}
+		
+		lastAngle += angle;
+		return smallest;
+	}
+
+	private static double angleAt(Integer[] integers, Integer[] integers2) {
+		
+		double tan1 = Math.atan2(integers2[1] - integers[1], integers2[0] - integers[0]);
+		
+		tan1 -= lastAngle;
+		
+		if (tan1 < 0)
+			tan1 += 2*Math.PI;
+		
+		return tan1;
+	}
+
+
 
 }
